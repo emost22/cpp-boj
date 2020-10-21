59c17fa5f09d9ff19728c60893110465dfba577e
ys <emost22@naver.com>
2020-10-21 22:44:07 +0900

    boj 2743 단어 길이 재기

---
+++ 문자열 처리/2743.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string str;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> str;
+
+	cout << str.length() << '\n';
+
+	return 0;
+}
