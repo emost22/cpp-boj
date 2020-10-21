47069b4073409368f073485917eae1b94041fdfb
ys <emost22@naver.com>
2020-10-21 21:51:54 +0900

    boj 2207 가위바위보

---
+++ 2-Sat/2207.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> list[20001];
+stack<int> st;
+int dis[20001], scc[20001], N, M, cnt, r;
+
+int dfs(int v) {
+	dis[v] = ++cnt;
+	int ret = dis[v];
+	st.push(v);
+	for (int next : list[v]) {
+		if (!dis[next]) ret = min(ret, dfs(next));
+		else if (!scc[next]) ret = min(ret, dis[next]);
+	}
+
+	if (ret == dis[v]) {
+		r++;
+		while (1) {
+			int x = st.top();
+			st.pop();
+
+			scc[x] = r;
+			if (x == v) break;
+		}
+	}
+
+	return ret;
+}
+
+void func() {
+	for (int i = 1; i <= M; i++) {
+		if (!dis[-i + 10000]) dfs(-i + 10000);
+		if (!dis[i + 10000]) dfs(i + 10000);
+	}
+
+	for (int i = 1; i <= M; i++) {
+		if (scc[-i + 10000] == scc[i + 10000]) {
+			cout << "OTL\n";
+			return;
+		}
+	}
+
+	cout << "^_^\n";
+}
+
+void input() {
+	int x, y;
+	cin >> N >> M;
+	while (N--) {
+		cin >> x >> y;
+
+		list[-x + 10000].push_back(y + 10000);
+		list[-y + 10000].push_back(x + 10000);
+	}
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+
+	return 0;
+}
