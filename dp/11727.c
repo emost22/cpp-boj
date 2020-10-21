7469c81c12b2f437ffa1fa9aac81b59b2a6493af
ys <emost22@naver.com>
2020-10-21 22:15:58 +0900

    boj 11727 2×n 타일링 2

---
+++ dp/11727.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int dp[1001];
+
+int find(int N) {
+	dp[1] = 1;
+	dp[2] = 3;
+	for (int i = 3; i <= N; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+	}
+
+	return dp[N];
+}
+
+int main() {
+	int N;
+
+	scanf("%d", &N);
+
+	printf("%d\n", find(N));
+
+	return 0;
+}
