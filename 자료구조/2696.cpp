c49a909418d06364e4ad6e9557b18491c8c8f905
ys <emost22@naver.com>
2020-11-29 17:21:44 +0900

    boj 2696 중앙값 구하기

---
+++ 자료구조/2696.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N, cnt;
+int list[10000];
+
+void func() {
+	priority_queue<int> lq, rq;
+	int lsize = 0;
+	int rsize = 0;
+
+	for (int i = 0; i < N; i++) {
+		if (lq.empty() && rq.empty()) {
+			rq.push(-list[i]);
+			rsize++;
+		}
+		else if (-rq.top() > list[i]) {
+			lq.push(list[i]);
+			lsize++;
+
+			if (lsize > rsize) {
+				lsize--;
+				rsize++;
+				rq.push(-lq.top());
+				lq.pop();
+			}
+		}
+		else {
+			rq.push(-list[i]);
+			rsize++;
+
+			if (rsize > lsize + 1) {
+				rsize--;
+				lsize++;
+				lq.push(-rq.top());
+				rq.pop();
+			}
+		}
+
+		if (i % 2 == 0) {
+			if (cnt == 10) {
+				cout << '\n';
+				cnt = 0;
+			}
+			cout << -rq.top() << ' ';
+			cnt++;
+		}
+	}
+	cout << '\n';
+}
+
+void input() {
+	cin >> N;
+	cout << (N + 1) / 2 << '\n';
+	for (int i = 0; i < N; i++) {
+		cin >> list[i];
+	}
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		input();
+		func();
+		cnt = 0;
+	}
+
+	return 0;
+}
