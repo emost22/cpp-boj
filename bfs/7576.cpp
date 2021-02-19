f5e287c42e14e882433496062fb71e53fe2f23cd
ys <emost22@naver.com>
2021-02-19 21:43:27 +0900

    boj 7576 토마토

--- bfs/7576.cpp
+++ bfs/7576.cpp
@@ -3,61 +3,72 @@
 using namespace std;
 
 queue<pair<int, int> > q;
-int map[1000][1000], visit[1000][1000];
-int x_direct[] = { 0,1,0,-1 }, y_direct[] = { 1,0,-1,0 };
+int direct[4][2] = { {0,1},{1,0},{0,-1},{-1,0} };
+bool visit[1001][1001];
+int list[1001][1001];
 int N, M, ans;
 
-void bfs(int qsize) {
-	for (int T = 0; T < qsize; T++) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
+void bfs() {
+	int t = 1;
+	for (; !q.empty(); t++) {
+		int qsize = q.size();
+		while (qsize--) {
+			int x = q.front().first;
+			int y = q.front().second;
+			q.pop();
 
-		for (int i = 0; i < 4; i++) {
-			int xx = x + x_direct[i];
-			int yy = y + y_direct[i];
+			for (int i = 0; i < 4; i++) {
+				int nx = x + direct[i][0];
+				int ny = y + direct[i][1];
+				
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+				if (visit[nx][ny] || list[nx][ny] == -1) continue;
 
-			if (xx < 0 || yy < 0 || xx >= N || yy >= M) continue;
-			if (visit[xx][yy]) continue;
-			if (!map[xx][yy]) {
-				map[xx][yy] = 1;
-				visit[xx][yy] = 1;
-				q.push(make_pair(xx, yy));
+				q.push({ nx,ny });
+				visit[nx][ny] = true;
+				ans--;
+			}
+		}
+
+		if (!ans) {
+			cout << t << '\n';
+			return;
+		}
+	}
+
+	cout << "-1\n";
+}
+
+void func() {
+	if (!ans) {
+		cout << "0\n";
+		return;
+	}
+
+	bfs();
+}
+
+void input() {
+	cin >> M >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> list[i][j];
+			if (!list[i][j])
+				ans++;
+			else if (list[i][j] == 1) {
+				q.push({ i,j });
+				visit[i][j] = true;
 			}
 		}
 	}
 }
 
 int main() {
-	cin.tie(NULL);
+	cin.tie(NULL); cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	cin >> M >> N;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == 1) {
-				q.push(make_pair(i, j));
-				visit[i][j] = 1;
-			}
-		}
-	}
-
-	while (!q.empty()) {
-		bfs(q.size());
-		ans++;
-	}
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (!map[i][j]) {
-				ans = 0;
-				break;
-			}
-		}
-	}
-
-	cout << ans - 1 << '\n';
+	input();
+	func();
 
 	return 0;
 }
