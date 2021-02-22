e5d45849f25928faecd1c61953259c9b5b7412bc
ys <emost22@naver.com>
2021-02-22 20:26:04 +0900

    boj 17299 오등큰수

--- 자료구조/17299.cpp
+++ 자료구조/17299.cpp
@@ -3,36 +3,33 @@
 using namespace std;
 
 stack<int> s;
+int list[1000001], num[1000001], ans[1000001];
 int N;
-int list[1000001], cnt[1000001], ans[1000001];
-
-void func() {
-	for (int i = N; i > 0; i--) {
-		while (!s.empty() && cnt[s.top()] <= cnt[list[i]]) s.pop();
-
-		if (s.empty()) {
-			ans[i] = -1;
-			s.push(list[i]);
-		}
-		else {
-			ans[i] = s.top();
-			s.push(list[i]);
-		}
-	}
-}
 
 void print() {
 	for (int i = 1; i <= N; i++) {
 		cout << ans[i] << ' ';
 	}
+
 	cout << '\n';
+}
+
+void func() {
+	for (int i = N; i > 0; i--) {
+		while (!s.empty() && num[list[i]] >= num[s.top()]) s.pop();
+
+		if (s.empty()) ans[i] = -1;
+		else ans[i] = s.top();
+
+		s.push(list[i]);
+	}
 }
 
 void input() {
 	cin >> N;
 	for (int i = 1; i <= N; i++) {
 		cin >> list[i];
-		cnt[list[i]]++;
+		num[list[i]]++;
 	}
 }
 
