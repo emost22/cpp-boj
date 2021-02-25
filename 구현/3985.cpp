70a56585508c1e5f1a5c3c8b39d173988a9cc456
ys <emost22@naver.com>
2021-02-25 20:59:03 +0900

    boj 3985 롤 케이크

--- 구현/3985.cpp
+++ 구현/3985.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int list[1001], num[1001];
 int L, N;
 
 void func() {
-	int ans = 0, ansidx = 0;
-	for (int i = 1; i <= L; i++) {
-		if (!list[i]) continue;
-		num[list[i]]++;
-	}
-
+	int maxlength = 0;
+	int maxidx = 0;
 	for (int i = 1; i <= N; i++) {
-		if (ans < num[i]) {
-			ans = num[i];
-			ansidx = i;
+		if (maxlength < num[i]) {
+			maxlength = num[i];
+			maxidx = i;
 		}
 	}
 
-	cout << ansidx << '\n';
+	cout << maxidx << '\n';
 }
 
 void input() {
-	int maxidx = 0, maxe = 0;
+	int maxlength = 0;
+	int maxidx = 0;
 	int l, r;
 	cin >> L >> N;
 	for (int i = 1; i <= N; i++) {
 		cin >> l >> r;
-		if (maxe < r - l) {
-			maxe = r - l;
+		if (maxlength < r - l) {
+			maxlength = r - l;
 			maxidx = i;
 		}
+
 		for (int j = l; j <= r; j++) {
 			if (list[j]) continue;
 			list[j] = i;
+			num[i]++;
 		}
 	}
 
