583985ff5bba1303283f6529a3a2923258d5afee
ys <emost22@naver.com>
2021-02-25 20:59:31 +0900

    boj 2304 창고 다각형

--- 세그먼트 트리/2304.cpp
+++ 세그먼트 트리/2304.cpp
@@ -2,34 +2,33 @@
 #include <algorithm>
 using namespace std;
 
-int N, L;
-int list[1001], tree[4004];
+int tree[4004], list[1001];
+int N, L, ans;
 
 int init(int node, int s, int e) {
-	if (s == e) {
-		return tree[node] = list[s];
-	}
+	if (s == e) return tree[node] = list[s];
+
 	int m = (s + e) / 2;
 	return tree[node] = max(init(node * 2, s, m), init(node * 2 + 1, m + 1, e));
 }
 
-int findmax(int node, int s, int e, int l, int r) {
+int query(int node, int s, int e, int l, int r) {
+	if (l > e || s > r) return 0;
 	if (l <= s && e <= r) return tree[node];
-	if (s > r || l > e) return 0;
 
 	int m = (s + e) / 2;
-	return max(findmax(node * 2, s, m, l, r), findmax(node * 2 + 1, m + 1, e, l, r));
+	return max(query(node * 2, s, m, l, r), query(node * 2 + 1, m + 1, e, l, r));
 }
 
 void func() {
-	int ans = 0;
 	for (int i = 1; i <= L; i++) {
-		int l = findmax(1, 1, L, 1, i);
-		int r = findmax(1, 1, L, i, L);
+		int l = query(1, 1, L, 1, i);
+		int r = query(1, 1, L, i, L);
 
 		if (list[i] < l || list[i] < r) {
 			list[i] = min(l, r);
 		}
+
 		ans += list[i];
 	}
 
@@ -39,7 +38,7 @@ void func() {
 void input() {
 	int l, h;
 	cin >> N;
-	for (int i = 1; i <= N; i++) {
+	for (int i = 0; i < N; i++) {
 		cin >> l >> h;
 		list[l] = h;
 		L = max(L, l);
