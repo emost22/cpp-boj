dc2f57d43e170a2289bf8b69c7e7b04ee0407a5b
ys <emost22@naver.com>
2021-02-27 22:50:06 +0900

    boj 1913 달팽이

---
+++ 구현/1913.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int list[1000][1000];
+int direct[4][2] = { {-1,0},{0,1},{1,0},{0,-1} };
+int N, K, ansx, ansy;
+
+void print() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cout << list[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+
+	cout << ansx << ' ' << ansy << '\n';
+}
+
+void func() {
+	int x = (N + 1) / 2;
+	int y = (N + 1) / 2;
+	int move = 1;
+	int cnt = 0;
+	int idx = 0;
+	int value = 1;
+	list[x][y] = value++;
+
+	while (true) {
+		x += direct[idx][0];
+		y += direct[idx][1];
+		cnt++;
+
+		if (value == K) {
+			ansx = x;
+			ansy = y;
+		}
+
+		list[x][y] = value++;
+		if (cnt == move) {
+			if (idx == 1 || idx == 3) move++;
+			idx = (idx + 1) % 4;
+			cnt = 0;
+		}
+		if (x == 1 && y == 1) break;
+	}
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	cin >> N >> K;
+	func();
+	print();
+
+	return 0;
+}
