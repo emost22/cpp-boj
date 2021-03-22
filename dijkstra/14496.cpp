2406c1289d1d9acf89074011deaadbd7fa66f31f
ys <emost22@naver.com>
2021-03-22 21:17:59 +0900

    boj 14496 그대, 그머가 되어

---
+++ dijkstra/14496.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#define INF 1000000000
+using namespace std;
+
+vector<pair<int, int> > list[1001];
+int d[1001];
+int N, M, start, des;
+
+void dijkstra() {
+	d[start] = 0;
+	priority_queue<pair<int, int> > q;
+	q.push({ start, 0 });
+	while (!q.empty()) {
+		int x = q.top().first;
+		int dis = -q.top().second;
+		q.pop();
+
+		if (d[x] < dis) continue;
+
+		for (int i = 0; i < list[x].size(); i++) {
+			int next = list[x][i].first;
+			int nextdis = dis + list[x][i].second;
+
+			if (d[next] > nextdis) {
+				d[next] = nextdis;
+				q.push({ next, -nextdis });
+			}
+		}
+	}
+	if (d[des] == INF) cout << "-1\n";
+	else cout << d[des] << '\n';
+}
+
+void init() {
+	for (int i = 1; i <= N; i++) {
+		d[i] = INF;
+	}
+}
+
+void input() {
+	int u, v;
+	cin >> start >> des >> N >> M;
+	init();
+
+	while (M--) {
+		cin >> u >> v;
+		list[u].push_back({ v,1 });
+		list[v].push_back({ u,1 });
+	}
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	dijkstra();
+
+	return 0;
+}
