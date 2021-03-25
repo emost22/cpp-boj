3ffcde5dbcb73016e2e69de57a686db622eba722
ys <emost22@naver.com>
2021-03-25 15:25:14 +0900

    boj 14003 가장 긴 증가하는 부분 수열 5

---
+++ binarysearch/14003.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v, ans;
+int list[1000001], lis[1000001];
+int N, length, lisidx;
+
+void print() {
+	reverse(ans.begin(), ans.end());
+	cout << ans.size() << '\n';
+	for (int i = 0; i < ans.size(); i++) {
+		cout << ans[i] << ' ';
+	}
+	cout << '\n';
+}
+
+void solve() {
+	length = v.size();
+	for (int i = N; i >= 1; i--) {
+		if (lis[i] == length) {
+			ans.push_back(list[i]);
+			length--;
+		}
+
+		if (!length) return;
+	}
+}
+
+void func() {
+	v.push_back(list[1]);
+	lisidx++;
+	lis[1] = 1;
+	for (int i = 2; i <= N; i++) {
+		if (v[v.size() - 1] < list[i]) {
+			v.push_back(list[i]);
+			lisidx++;
+			lis[i] = lisidx;
+		}
+		else {
+			int tmp = lower_bound(v.begin(), v.end(), list[i]) - v.begin();
+
+			v[tmp] = list[i];
+			lis[i] = tmp + 1;
+		}
+	}
+}
+
+void input() {
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> list[i];
+	}
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+	solve();
+	print();
+
+	return 0;
+}
