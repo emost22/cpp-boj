b162f887caed9c53c131ee0e7d3a20cb4b99dfa8
ysys <emoney96@naver.com>
2021-09-05 11:55:51 +0900

    boj 11382 꼬마 정민

---
+++ 수학/11382.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+ll x, y, z;
+
+void input() {
+	cin >> x >> y >> z;
+	cout << x + y + z << '\n';
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+
+	return 0;
+}
