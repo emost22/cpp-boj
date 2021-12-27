53a6b675de9324715f5ab1cc2d268ec26f96f413
ysys <emoney96@naver.com>
2021-12-27 21:56:43 +0900

    boj 2168 타일 위의 대각선

---
+++ 정수론/2168.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int x, int y) {
+	return y ? gcd(y, x % y) : x;
+}
+
+void input() {
+	int x, y;
+	cin >> x >> y;
+	
+	cout << x + y - gcd(x, y) << '\n';
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+
+	return 0;
+}
