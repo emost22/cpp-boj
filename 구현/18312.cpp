e62cb61b8fe24a116d863bf0af7a52ea9c7255e6
ysys <emoney96@naver.com>
2022-01-17 20:36:34 +0900

    boj 18312 시각

---
+++ 구현/18312.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef struct Time {
+	int h;
+	int m;
+	int s;
+}Time;
+
+int N, K;
+
+void func() {
+	Time e = { N, 59, 59 };
+
+	int ans = 0;
+	for (int i = 0; i <= N; i++) {
+		for (int j = 0; j < 60; j++) {
+			for (int k = 0; k < 60; k++) {
+				int ii = i / 10;
+				int jj = j / 10;
+				int kk = k / 10;
+
+				if (i % 10 == K || ii == K || j % 10 == K || jj == K || k % 10 == K || kk == K) {
+					ans++;
+				}
+			}
+		}
+	}
+
+	cout << ans << '\n';
+}
+
+void input() {
+	cin >> N >> K;
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+
+	return 0;
+}
