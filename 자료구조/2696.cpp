dce4285697b79cd5b14499faac986a4d98f065d5
ysys <emoney96@naver.com>
2022-05-09 19:00:01 +0900

    Fix: boj 2696 중앙값 구하기
    + rq를 최소 힙으로 변경한다.
    + rq에 -list[i]가 아닌 list[i]를 push한다.

--- 자료구조/2696.cpp
+++ 자료구조/2696.cpp
@@ -1,39 +1,47 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 
 int N, cnt;
 int list[10000];
 
+struct cmp {
+	bool operator()(int a, int b) {
+		return a > b;
+	}
+};
+
 void func() {
-	priority_queue<int> lq, rq;
+	priority_queue<int> lq;
+	priority_queue<int, vector<int>, cmp> rq;
 	int lsize = 0;
 	int rsize = 0;
 
 	for (int i = 0; i < N; i++) {
 		if (lq.empty() && rq.empty()) {
-			rq.push(-list[i]);
+			rq.push(list[i]);
 			rsize++;
 		}
-		else if (-rq.top() > list[i]) {
+		else if (rq.top() > list[i]) {
 			lq.push(list[i]);
 			lsize++;
 
 			if (lsize > rsize) {
 				lsize--;
 				rsize++;
-				rq.push(-lq.top());
+				rq.push(lq.top());
 				lq.pop();
 			}
 		}
 		else {
-			rq.push(-list[i]);
+			rq.push(list[i]);
 			rsize++;
 
 			if (rsize > lsize + 1) {
 				rsize--;
 				lsize++;
-				lq.push(-rq.top());
+				lq.push(rq.top());
 				rq.pop();
 			}
 		}
@@ -43,7 +51,7 @@ void func() {
 				cout << '\n';
 				cnt = 0;
 			}
-			cout << -rq.top() << ' ';
+			cout << rq.top() << ' ';
 			cnt++;
 		}
 	}
