626ab5678ea338205c07c09607a19ef9f2d63eea
ysys <emoney96@naver.com>
2022-06-11 13:08:44 +0900

    boj 3079 입국심사

---
+++ binarysearch/3079.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 100001
+using namespace std;
+typedef long long ll;
+
+ll list[MAX];
+ll M, maxTime;
+int N;
+
+void func() {
+	ll l = 0;
+	ll r = maxTime * M;
+	ll ans = 0;
+	while (l <= r) {
+		ll m = (l + r) / 2;
+
+		ll sum = 0;
+		for (int i = 0; i < N; i++) {
+			sum += (m / list[i]);
+		}
+
+		if (sum >= M) 
+		{
+			ans = m;
+			r = m - 1;
+		}
+		else {
+			l = m + 1;
+		}
+	}
+
+	cout << ans << '\n';
+}
+
+void input() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> list[i];
+		maxTime = max(maxTime, list[i]);
+	}
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+
+	return 0;
+}
