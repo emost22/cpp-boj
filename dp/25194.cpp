ddade2e1b862fc29052ba9dd12dc80876fb13cb7
ysys <emoney96@naver.com>
2022-06-14 14:16:43 +0900

    boj 25194 결전의 금요일

---
+++ dp/25194.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#define MAX 1001
+using namespace std;
+
+int dp[MAX][7];
+int list[MAX];
+int N;
+
+int dfs(int idx, int d) {
+	if (d == 5) return 1;
+	if (idx == N) return 0;
+
+	int& ret = dp[idx][d];
+	if (ret != -1) return ret;
+	ret = 0;
+
+	for (int i = idx; i < N; i++) {
+		ret |= dfs(i + 1, (d + list[i]) % 7);
+		if (ret) return ret;
+	}
+
+	return ret;
+}
+
+void func() {
+	memset(dp, -1, sizeof(dp));
+	cout << (dfs(0, 1) ? "YES\n" : "NO\n");
+}
+
+void input() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> list[i];
+	}
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+
+	return 0;
+}
