756d5fcf037c7658a371a502fa7fe9c00eb07573
ysys <emoney96@naver.com>
2022-06-14 23:44:53 +0900

    boj 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ 구현/18108.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+void input() {
+	cin >> N;
+	cout << N - 543 << '\n';
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+
+	return 0;
+}
