9f49778091c640d67dd76fd743fef2f0ff42acf5
ysys <emoney96@naver.com>
2022-06-28 13:45:12 +0900

    boj 2295 세 수의 합

--- binarysearch/2295.cpp
+++ binarysearch/2295.cpp
@@ -16,13 +16,31 @@ void func() {
 	}
 	sort(v.begin(), v.end());
 
+	int vsize = v.size();
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			int dc = list[i] - list[j];
 			if (dc <= 0) continue;
 
-			int cnt = upper_bound(v.begin(), v.end(), dc) - lower_bound(v.begin(), v.end(), dc);
-			if (cnt) {
+			int l = 0;
+			int r = vsize - 1;
+			bool flag = false;
+			while (l <= r) {
+				int m = (l + r) / 2;
+
+				if (v[m] == dc) {
+					flag = true;
+					break;
+				}
+				else if (v[m] > dc) {
+					r = m - 1;
+				}
+				else {
+					l = m + 1;
+				}
+			}
+
+			if (flag) {
 				cout << list[i] << '\n';
 				return;
 			}
