64cd20a8597263664d4b60c6fccf689d8c8405e9
ysys <emoney96@naver.com>
2022-07-12 23:17:55 +0900

    boj 1402 아무래도이문제는A번난이도인것같다

---
+++ ad-hoc/1402.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void input() {
+	int x, y;
+	cin >> x >> y;
+	cout << "yes\n";
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		input();
+	}
+
+	return 0;
+}
