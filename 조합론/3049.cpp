eb6036cd90c51a3ba98bfa3c53b54a5f3d63d158
ysys <emoney96@naver.com>
2022-07-16 23:43:27 +0900

    boj 3049 다각형의 대각선

---
+++ 조합론/3049.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+void func() {
+	cout << N * (N - 1) * (N - 2) * (N - 3) / 24 << '\n';
+}
+
+void input() {
+	cin >> N;
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	input();
+	func();
+
+	return 0;
+}
