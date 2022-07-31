08889fe120b4041970569a798fdc3c2672b49898
ysys <emoney96@naver.com>
2022-08-01 00:00:58 +0900

    boj 24086 身長 (Height)

---
+++ 수학/24086.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void input() {
+	int x, y;
+	cin >> x >> y;
+	cout << y - x << '\n';
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+
+	return 0;
+}
