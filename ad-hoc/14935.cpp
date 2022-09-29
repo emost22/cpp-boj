835bac490b8a712ec799f3fb87bbb56b31544c86
emost22 <emoney96@naver.com>
2022-09-29 09:13:44 +0900

    boj 14935 FA

---
+++ ad-hoc/14935.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void input() {
+	string str;
+	cin >> str;
+	cout << "FA\n";
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+
+	return 0;
+}
