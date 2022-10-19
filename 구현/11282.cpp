a4724ee0849df83612f17ce34881836e949d2c8f
emost22 <emoney96@naver.com>
2022-10-19 09:32:32 +0900

    boj 11282 한글

---
+++ 구현/11282.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string str;
+int N;
+
+void func() {
+	N += 3071;
+	str.push_back(N / 4096 + 234);
+	str.push_back(N / 64 % 64 + 128);
+	str.push_back(N % 64 + 128);
+	cout << str << '\n';
+}
+
+void input() {
+	cin >> N;
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+
+	return 0;
+}
