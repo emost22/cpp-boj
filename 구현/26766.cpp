d040210f69d7c1dde0120951268cc55be5074cf8
emost22 <emoney96@naver.com>
2022-12-24 16:21:27 +0900

    boj 26766 Serca

---
+++ 구현/26766.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string str = " @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n";
+int N;
+
+void func() {
+	while (N--) {
+		cout << str;
+	}
+}
+
+void input() {
+	cin >> N;
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+
+	return 0;
+}
