3433f3da0126411703e76053cd1217a58f41722d
emost22 <emoney96@naver.com>
2023-01-02 09:29:39 +0900

    boj 17129 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유

---
+++ bfs/17129.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <queue>
+#define MAX 3001
+using namespace std;
+typedef pair<int, int> pii;
+
+char list[MAX][MAX];
+bool visit[MAX][MAX];
+int direct[4][2] = { {0,1},{1,0},{0,-1},{-1,0} };
+pii s;
+int N, M;
+
+int bfs(int sx, int sy) {
+	queue<pii> q;
+	q.push({ sx,sy });
+	visit[sx][sy] = true;
+	for (int t = 0; !q.empty(); t++) {
+		int qsize = q.size();
+		while (qsize--) {
+			int x = q.front().first;
+			int y = q.front().second;
+			q.pop();
+
+			if (list[x][y] > '2') {
+				return t;
+			}
+
+			for (int d = 0; d < 4; d++) {
+				int nx = x + direct[d][0];
+				int ny = y + direct[d][1];
+
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+				if (list[nx][ny] == '1' || visit[nx][ny]) continue;
+
+				q.push({ nx,ny });
+				visit[nx][ny] = true;
+			}
+		}
+	}
+
+	return -1;
+}
+
+void init() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (list[i][j] == '2') {
+				s = { i,j };
+				return;
+			}
+		}
+	}
+}
+
+void func() {
+	init();
+	int ret = bfs(s.first, s.second);
+	if (ret != -1) {
+		cout << "TAK\n" << ret << '\n';
+	}
+	else {
+		cout << "NIE\n";
+	}
+}
+
+void input() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> list[i];
+	}
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+
+	return 0;
+}
