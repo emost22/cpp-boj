5465429d5b688578208b27784a8a776f3c6a7687
emost22 <emoney96@naver.com>
2023-06-15 09:31:01 +0900

    boj 24264 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ 구현/24264.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+ll N;
+
+void func() {
+	cout << N * N << "\n2\n";
+}
+
+void input() {
+	cin >> N;
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+
+	return 0;
+}
