c05954e14788f1f4f09a9811891057a83daaa816
emost22 <emoney96@naver.com>
2023-07-06 15:37:23 +0900

    boj 12039 Polynesiaglot (Large)

---
+++ dp/12039.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#define MAX 501
+#define MOD 1000000007
+using namespace std;
+typedef long long ll;
+
+ll dp[MAX][2];
+ll C, V, L;
+
+void func(int tc) {
+	dp[1][0] = V;
+	for (int i = 2; i <= L; i++) {
+		dp[i][0] = (dp[i - 1][0] * V % MOD + dp[i - 1][1] * V % MOD) % MOD;
+		dp[i][1] = (dp[i - 1][0] * C) % MOD;
+	}
+
+	cout << "Case #" << tc << ": " << (dp[L][0] + dp[L][1]) % MOD << '\n';
+}
+
+void input() {
+	cin >> C >> V >> L;
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int tc;
+	cin >> tc;
+	for (int t = 1; t <= tc; t++) {
+		input();
+		func(t);
+	}
+
+	return 0;
+}
