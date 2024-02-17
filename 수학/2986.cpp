09e666ce00f7cb4a98438058c0e378461ced845d
emost22 <emoney96@naver.com>
2024-02-17 21:03:54 +0900

    boj 2986 파스칼

---
+++ 수학/2986.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+void func() {
+	for (int i = 2; i * i <= N; i++) {
+		if (!(N % i)) {
+			cout << N - N / i << '\n';
+			return;
+		}
+	}
+
+	cout << N - 1 << '\n';
+}
+
+void input() {
+	cin >> N;
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+
+	return 0;
+}
