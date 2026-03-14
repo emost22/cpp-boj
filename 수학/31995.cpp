d4d317dbd7b1a9d66a1518393bc766c496180c5b
emost22 <emoney96@naver.com>
2026-03-14 18:07:41 +0900

    boj 31995 게임말 올려놓기

---
+++ 수학/31995.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+
+void func() {
+	cout << ((N - 1) * (M - 1) << 1) << '\n';
+}
+
+void input() {
+	cin >> N >> M;
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	input();
+	func();
+
+	return 0;
+}
