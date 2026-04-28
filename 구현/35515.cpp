54e0599bf2e8a87fc4a1be98deb14650f92f9d28
emost22 <emoney96@naver.com>
2026-04-28 19:34:58 +0900

    boj 35515 Good Bye, BOJ!

---
+++ 구현/35515.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void func() {
+	cout << "BOJBaekjoon¹éÁØ\n";
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	func();
+
+	return 0;
+}
